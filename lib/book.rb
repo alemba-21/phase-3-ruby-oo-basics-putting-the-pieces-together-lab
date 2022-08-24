2478fed63a396f4f45ddd06b5e134b8da7994800
alemba-21 <alembakiragu@gmail.com>
2022-08-24 15:09:11 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
+   
+    def author
+       author = "Agatha Christie"
+    end
+   
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+      
+     end   
+   
+       def initialize(title)
+           @title = title
+           end    
+            
+   end
+   
+   b1 = Book.new('shirika la leri la rwanda')
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def brand
+        brand = 'Nike'
+    end
+def cobble 
+    @condition =  'new'
+    puts "Your shoe is as good as new!"
+    
+end
+
+    def initialize (brand)
+        @brand
+        
+    end
+end
+s1 = Shoe.new('Jordans')
